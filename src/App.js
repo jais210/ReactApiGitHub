3822e7f27fb4db902d05463ba08160426ba9c5ea
jais210 <jakeline_210@hotmail.com>
2017-10-12 16:12:08 -0500

    dos componentes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { FormGroup } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { ControlLabel } from "react-bootstrap";
+import { InputGroup } from "react-bootstrap";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink,
+  NavNavLink,
+  Redirect
+} from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h2>
+      <center>
+        <strong>Github Battle: Battle your friends... and stuff.</strong>
+      </center>
+    </h2>
+    <center>
+      <Button bsStyle="primary" bsSize="large">
+        Battle
+      </Button>
+    </center>
+  </div>
+);
+
+const Battle = () => (
+  <center>
+    <Form inline>
+      <FormGroup controlId="formInlineName">
+        <InputGroup>
+          <Col sm={8} mdOffset={4}>
+            <h4>Player One</h4>
+            <FormControl type="text" placeholder="Github user" />
+            <Button type="submit" bsSize="small">
+              Submit
+            </Button>
+          </Col>
+        </InputGroup>
+      </FormGroup>
+      <FormGroup controlId="formInlineName">
+        <InputGroup>
+          <Col sm={8} mdOffset={4}>
+            <h4>Player Two</h4>
+            <FormControl type="text" placeholder="Github user" />
+            <Button type="submit" bsSize="small">
+              Submit
+            </Button>
+          </Col>
+        </InputGroup>
+      </FormGroup>
+    </Form>
+  </center>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Holi Ale, si ves esto es porque necesito AYUDA</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div>
+          <Home />
+        </div>
+        <div>
+        <Battle />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Button } from 'react-bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
